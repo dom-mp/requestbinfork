eabaa021122cbfeb08489933c2ea24f7a5644631
Daniel Chae <daniel.jiwoong@gmail.com>
2025-05-10 06:32:03 +0900

    basic nav bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
+import Nav from "./components/Nav";
+
 function App() {
   return (
     <>
-      <h2>RequestBin</h2>
+      <Nav />
     </>
   );
 }
---
+++ frontend/src/components/Nav/Nav.tsx
@@ -0,0 +1,25 @@
+import "./Nav.css";
+
+const Nav = () => {
+  return (
+    <nav>
+      <menu>
+        <li>
+          <a href="/">RequestBin</a>
+        </li>
+        <li>
+          <a href="https://github.com/dom-and-the-night-owls/requestbin">
+            github
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/dom-and-the-night-owls/requestbin/wiki">
+            documentation
+          </a>
+        </li>
+      </menu>
+    </nav>
+  );
+};
+
+export default Nav;
