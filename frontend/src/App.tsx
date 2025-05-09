6ac3f8d8dd291ca07b0b18bd9ac62718769deeea
Daniel Chae <daniel.jiwoong@gmail.com>
2025-05-10 07:19:56 +0900

    Baskets component initial

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,20 @@
+import { useState, useEffect } from "react";
+
 import Nav from "./components/Nav";
+import Baskets from "./components/Baskets";
 
 function App() {
+  const [baskets, setBaskets] = useState<Array<string>>([]);
+
+  useEffect(() => {
+    const mockBaskets = ["9dj210f", "dt2u38h", "nlg99ed"];
+    setBaskets(mockBaskets);
+  }, []);
+
   return (
     <>
       <Nav />
+      <Baskets baskets={baskets} />
     </>
   );
 }
