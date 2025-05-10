c2b463723bf4712a90c6c27f05b9d782d1745a07
Daniel Chae <daniel.jiwoong@gmail.com>
2025-05-10 11:06:14 +0900

    FEAT!: react-router implemented

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,17 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 import Nav from "./components/Nav";
 import Baskets from "./components/Baskets";
 import CreateBasket from "./components/CreateBasket";
+
+// temporary Basket component placeholder
+const Basket = () => <></>;
 
 function App() {
   const [baskets, setBaskets] = useState<Array<string>>([]);
 
   useEffect(() => {
-    const mockBaskets = ["9dj210f", "dt2u38h", "nlg99ed"];
+    const mockBaskets = ["9dj210f", "dt2u38h", "nlg23ed"];
     setBaskets(mockBaskets);
   }, []);
 
@@ -16,7 +19,13 @@ function App() {
     <BrowserRouter>
       <Nav />
       <Baskets baskets={baskets} />
-      <CreateBasket />
+
+      <Routes>
+        <Route path="/" element={<CreateBasket />} />
+        <Route path="baskets">
+          <Route path=":basketName" element={<Basket />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
