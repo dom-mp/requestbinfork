b0519a6fedc08efeb4fbad9d6ff972cf2d7ee7e1
Daniel Chae <daniel.jiwoong@gmail.com>
2025-05-11 07:57:51 +0900

    CHORE: mui implemented in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,12 @@ import Basket from "./components/Basket";
 import Baskets from "./components/Baskets";
 import CreateBasket from "./components/CreateBasket";
 import "./App.css";
+import { Container } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 function App() {
   const [baskets, setBaskets] = useState<Array<string>>([]);
@@ -16,17 +22,20 @@ function App() {
   }, []);
 
   return (
-    <BrowserRouter>
-      <Nav />
-      <Baskets baskets={baskets} />
+    <Container maxWidth="sm">
+      <CssBaseline />
+      <BrowserRouter>
+        <Nav />
+        <Baskets baskets={baskets} />
 
-      <Routes>
-        <Route path="/" element={<CreateBasket setBaskets={setBaskets} />} />
-        <Route path="baskets">
-          <Route path=":basketName" element={<Basket />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<CreateBasket setBaskets={setBaskets} />} />
+          <Route path="baskets">
+            <Route path=":basketName" element={<Basket />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Container>
   );
 }
 
