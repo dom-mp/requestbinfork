a07c8e27f32cade0dbf56ee056bbed2cd3c45683
Daniel Singer <dansinger@pm.me>
2025-05-10 16:18:21 +1000

    added mongo schema & connection

--- backend/src/controllers/mongo.ts
+++ backend/src/controllers/mongo.ts
@@ -12,4 +12,12 @@ mongoose.connect(databaseUrl)
     request: Object
   })
 
+  requestBodySchema.set('toJSON', {
+    transform: (_document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
+
   export default mongoose.model('requestBody', requestBodySchema)
