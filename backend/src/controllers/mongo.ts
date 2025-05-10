0871d27a98ea50340b92fc2c481ea3b8b05e1502
Daniel Singer <dansinger@pm.me>
2025-05-11 05:05:11 +1000

    fixed typo

--- backend/src/controllers/mongo.ts
+++ backend/src/controllers/mongo.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const databaseUrl = 'mongodb://localhost:27017/requestBodies?'
+const databaseUrl = 'mongodb://localhost:27017/requestBodies'
 
 mongoose.set('strictQuery', false)
 
