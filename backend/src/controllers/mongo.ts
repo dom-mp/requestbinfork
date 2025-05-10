e13009530bd31163d3d7d3a274572ceebbf13075
Daniel Singer <dansinger@pm.me>
2025-05-10 16:30:19 +1000

    fixed some formatting mistakes

--- backend/src/controllers/mongo.ts
+++ backend/src/controllers/mongo.ts
@@ -8,16 +8,16 @@ mongoose.connect(databaseUrl)
   .then(() => console.log('connected to MongoDB'))
   .catch(error => console.log(`error connection to ${error.message}`))
 
-  const requestBodySchema = new mongoose.Schema({
-    request: Object
-  })
+const requestBodySchema = new mongoose.Schema({
+  request: Object
+})
 
-  requestBodySchema.set('toJSON', {
-    transform: (_document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
+requestBodySchema.set('toJSON', {
+  transform: (_document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
-  export default mongoose.model('requestBody', requestBodySchema)
+export default mongoose.model('requestBody', requestBodySchema)
