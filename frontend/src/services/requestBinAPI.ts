9b3740730d410fbead42f5f13dc360d2e5a8fb2e
Sekiel <im.sekiel@gmail.com>
2025-05-14 16:08:56 -0700

    feat: Local Storage now stores keys

--- frontend/src/services/requestBinAPI.ts
+++ frontend/src/services/requestBinAPI.ts
@@ -10,11 +10,12 @@ const getBaskets = async (): Promise<Array<string>> => {
   return response.data.baskets;
 };
 
-const getToken = async (): Promise<string> => {
-  const response = await axios.get(`${API_BASE}/generate_token`);
+const getToken = async (basketName: string): Promise<string> => {
+  const response = await axios.get(`${API_BASE}/generate_token/`, {
+    params: { name: basketName },
+  });
   return response.data.token;
 };
-
 const generateName = async (): Promise<string> => {
   const response = await axios.get(`${API_BASE}/generate_name`);
   return response.data.basketName;
@@ -31,7 +32,7 @@ const deleteBasket = async (basketName: string): Promise<void> => {
 
 const getRequests = async (basketName: string): Promise<Array<Request>> => {
   const response = await axios.get(
-    `${API_BASE}/baskets/${basketName}/requests`,
+    `${API_BASE}/baskets/${basketName}/requests`
   );
 
   const requests = response.data.requests;
