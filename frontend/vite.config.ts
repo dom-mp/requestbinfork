afab3638e0e9b097321d93d2de9ca52630772b66
Daniel Chae <daniel.jiwoong@gmail.com>
2025-05-15 03:28:55 +0900

    FIX: add hook proxy to vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,6 +7,10 @@ export default defineConfig({
 
   server: {
     proxy: {
+      "/hook": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
       "/api": {
         target: "http://localhost:3000",
         changeOrigin: true,
