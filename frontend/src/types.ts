ffdc3ec05afaa3094a7944bb0c55c3568357f6c5
Daniel Chae <daniel.jiwoong@gmail.com>
2025-05-17 17:19:46 +0900

    FEAT: Added zod type schema

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,7 +1,19 @@
-export interface Request {
-  basketName: string;
-  method: string;
-  sentAt: string;
-  headers: string;
-  requestBody?: string;
-}
+import { z } from "zod";
+
+export const RequestSchema = z.object({
+  basketName: z.string(),
+  method: z.string(),
+  sentAt: z.string(),
+  headers: z.string(),
+  requestBody: z.string().nullable(),
+});
+
+// export interface Request {
+//   basketName: string;
+//   method: string;
+//   sentAt: string;
+//   headers: string;
+//   requestBody?: string;
+// }
+
+export type Request = z.infer<typeof RequestSchema>;
